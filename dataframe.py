7f30255d3f343f64c6498d9d8220c53a36ce386b
polinchakb <william.polinchak@windstream.com>
2019-05-24 09:25:37 -0400

    Renamed app.py to dataframe.py.

--- dataframe.py
+++ dataframe.py
@@ -3,10 +3,6 @@ import numpy as np
 
 from matplotlib import pyplot
 
-series1 = pd.Series([10,20,30,40])
-
-print(series1)
-
 a = pd.DataFrame(np.random.rand(15,4), columns=['v','w','x','y'])
 
 a.plot.area()
